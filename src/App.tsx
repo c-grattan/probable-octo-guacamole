957aab66ad2d7af01d4040196f2dc4c30cd261c9
c-grattan <connorpgrattan@gmail.com>
2022-10-15 21:27:36 +0100

    Add sidebar

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,36 @@
-import React from 'react';
+import {useState} from "react";
 
 import {AppBar} from '@mui/material';
 import Toolbar from '@mui/material/Toolbar';
 import Typography from '@mui/material/Typography';
 import IconButton from '@mui/material/IconButton';
 import MenuIcon from '@mui/icons-material/Menu';
+import Drawer from '@mui/material/Drawer';
 
 function App() {
-  return (
-    <div>
-      <header>
-		<AppBar>
-			<Toolbar>
-				<IconButton><MenuIcon /></IconButton>
-				<Typography variant="h6">probable-octo-guacamole</Typography>
-			</Toolbar>
-		</AppBar>
-      </header>
-    </div>
-  );
+	const [sidebarOpen, setSidebarOpen] = useState(false);
+
+	function toggleSidebar() {
+		setSidebarOpen(!sidebarOpen)
+	}
+
+	return (
+		<div>
+			<header>
+				<AppBar>
+					<Toolbar>
+						<IconButton onClick={toggleSidebar}><MenuIcon /></IconButton>
+						<Typography variant="h4">probable-octo-guacamole</Typography>
+					</Toolbar>
+				</AppBar>
+				<Drawer open={sidebarOpen}
+						onClose={toggleSidebar}
+						anchor='left'>
+					<Typography variant="h3">Sidebar Menu</Typography>
+				</Drawer>
+			</header>
+		</div>
+	);
 }
 
 export default App;
