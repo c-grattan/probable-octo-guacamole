b5409fc175347aa57c886daf25bbb4db95778b83
c-grattan <connorpgrattan@gmail.com>
2022-10-17 20:08:49 +0100

    Refactor app header into new component

--- src/App.tsx
+++ src/App.tsx
@@ -1,43 +1,10 @@
-import {useState} from "react";
-
-import Box from '@mui/material/Box';
-import {AppBar} from '@mui/material';
-import Toolbar from '@mui/material/Toolbar';
-import Typography from '@mui/material/Typography';
-import IconButton from '@mui/material/IconButton';
-import MenuIcon from '@mui/icons-material/Menu';
-import Drawer from '@mui/material/Drawer';
+import { AppHeader } from "./components/AppHeader";
 
 function App() {
-	const [sidebarOpen, setSidebarOpen] = useState(false);
-
-	function toggleSidebar() {
-		setSidebarOpen(!sidebarOpen)
-	}
-
 	return (
 		<div>
 			<header>
-				<AppBar>
-					<Toolbar>
-						<IconButton
-							onClick={toggleSidebar}
-							size="medium"
-							edge="start"
-							sx={{
-								mr: 2
-							}}
-						>
-							<MenuIcon />
-						</IconButton>
-						<Typography variant="h4">Probable octo guacamole</Typography>
-					</Toolbar>
-				</AppBar>
-				<Drawer open={sidebarOpen}
-						onClose={toggleSidebar}
-						anchor='left'>
-					<Typography variant="h3">Sidebar Menu</Typography>
-				</Drawer>
+				<AppHeader />
 			</header>
 		</div>
 	);
---
+++ src/components/AppHeader.tsx
@@ -0,0 +1,43 @@
+import {useState} from "react";
+
+import {AppBar} from '@mui/material';
+import Toolbar from '@mui/material/Toolbar';
+import Typography from '@mui/material/Typography';
+import IconButton from '@mui/material/IconButton';
+import MenuIcon from '@mui/icons-material/Menu';
+import Drawer from '@mui/material/Drawer';
+
+export const AppHeader = () => {
+	const [sidebarOpen, setSidebarOpen] = useState(false);
+
+	function toggleSidebar() {
+		setSidebarOpen(!sidebarOpen)
+	}
+
+	return	<>
+				<AppBar>
+					<Toolbar>
+						<IconButton
+							onClick={toggleSidebar}
+							size="medium"
+							edge="start"
+							sx={{
+								mr: 2
+							}}
+						>
+							<MenuIcon />
+						</IconButton>
+
+						<Typography variant="h4">
+							Probable octo guacamole
+						</Typography>
+					</Toolbar>
+				</AppBar>
+				
+				<Drawer open={sidebarOpen}
+						onClose={toggleSidebar}
+						anchor='left'>
+					<Typography variant="h3">Sidebar Menu</Typography>
+				</Drawer>
+			</>
+}
