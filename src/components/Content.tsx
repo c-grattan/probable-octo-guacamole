372347bcad790dca088adcd145cdcde1a4c382c8
c-grattan <connorpgrattan@gmail.com>
2022-10-18 11:03:28 +0100

    Add button to app content

--- src/components/Content.tsx
+++ src/components/Content.tsx
@@ -1,4 +1,18 @@
+import Button from "@mui/material/Button";
+import Grid from "@mui/material/Grid";
+import IconButton from "@mui/material/IconButton";
+import Add from "@mui/icons-material/Add"
+
 export const Content = () => {
 	return <>
+		<Grid container spacing={2}
+						justifyContent="center"
+						alignContent="center">
+			<Grid item spacing={1}>
+				<IconButton size="large">
+					<Add />
+				</IconButton>
+			</Grid>
+		</Grid>
 	</>;
 }
